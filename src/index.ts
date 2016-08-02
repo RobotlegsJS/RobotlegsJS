6460b5de58535ea711ea0a1ac25d5486c9c53ed3
Tiago Schenkel <tschenkel@goodgamestudios.com>
2016-08-02 18:03:39 +0200

    Organising export clausules.

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,51 @@
-/**
- * API
- */
-export { IBundle                } from "./robotlegs/bender/framework/api/IBundle"
-export { IInjector              } from "./robotlegs/bender/framework/api/IInjector"
-export { IConfig                } from "./robotlegs/bender/framework/api/IConfig"
-export { IContext               } from "./robotlegs/bender/framework/api/IContext"
-export { IExtension             } from "./robotlegs/bender/framework/api/IExtension"
-export { IGuard                 } from "./robotlegs/bender/framework/api/IGuard"
-export { IHook                  } from "./robotlegs/bender/framework/api/IHook"
-export { ILifecycle             } from "./robotlegs/bender/framework/api/ILifecycle"
-export { ILogger                } from "./robotlegs/bender/framework/api/ILogger"
-export { ILogTarget             } from "./robotlegs/bender/framework/api/ILogTarget"
-export { IMatcher               } from "./robotlegs/bender/framework/api/IMatcher"
-export { LifecycleError         } from "./robotlegs/bender/framework/api/LifecycleError"
-export { LifecycleEvent         } from "./robotlegs/bender/framework/api/LifecycleEvent"
-// export { LifecycleState         } from "./bender/framework/api/LifecycleState"
-export { LogLevel               } from "./robotlegs/bender/framework/api/LogLevel"
-export { PinEvent               } from "./robotlegs/bender/framework/api/PinEvent"
+/// <reference path="../node_modules/inversify-dts/inversify/inversify.d.ts" />
+/// <reference path="../node_modules/reflect-metadata/reflect-metadata.d.ts" />
 
 /**
- * Implementation
+ * Framework API
  */
-export { Context                } from "./robotlegs/bender/framework/impl/Context"
-export { Logger                 } from "./robotlegs/bender/framework/impl/Logger"
-export { LogManager             } from "./robotlegs/bender/framework/impl/LogManager"
-export { Pin                    } from "./robotlegs/bender/framework/impl/Pin"
-export { ExtensionInstaller     } from "./robotlegs/bender/framework/impl/ExtensionInstaller"
-export { ConfigManager          } from "./robotlegs/bender/framework/impl/ConfigManager"
-export { ObjectProcessor        } from "./robotlegs/bender/framework/impl/ObjectProcessor"
-export { Lifecycle              } from "./robotlegs/bender/framework/impl/Lifecycle"
-export { LifecycleTransition    } from "./robotlegs/bender/framework/impl/LifecycleTransition"
-export { MessageDispatcher      } from "./robotlegs/bender/framework/impl/MessageDispatcher"
-export { safelyCallBack         } from "./robotlegs/bender/framework/impl/safelyCallBack"
-export { UID                    } from "./robotlegs/bender/framework/impl/UID"
-export { RobotlegsInjector      } from "./robotlegs/bender/framework/impl/RobotlegsInjector"
+export { IBundle                } from "./robotlegs/bender/framework/api/IBundle";
+export { IConfig                } from "./robotlegs/bender/framework/api/IConfig";
+export { IContext               } from "./robotlegs/bender/framework/api/IContext";
+export { IExtension             } from "./robotlegs/bender/framework/api/IExtension";
+export { IGuard                 } from "./robotlegs/bender/framework/api/IGuard";
+export { IHook                  } from "./robotlegs/bender/framework/api/IHook";
+export { IInjector              } from "./robotlegs/bender/framework/api/IInjector";
+export { ILifecycle             } from "./robotlegs/bender/framework/api/ILifecycle";
+export { ILogger                } from "./robotlegs/bender/framework/api/ILogger";
+export { ILogTarget             } from "./robotlegs/bender/framework/api/ILogTarget";
+export { IMatcher               } from "./robotlegs/bender/framework/api/IMatcher";
+export { LifecycleError         } from "./robotlegs/bender/framework/api/LifecycleError";
+export { LifecycleEvent         } from "./robotlegs/bender/framework/api/LifecycleEvent";
+export { LifecycleState         } from "./robotlegs/bender/framework/api/LifecycleState";
+export { LogLevel               } from "./robotlegs/bender/framework/api/LogLevel";
+export { PinEvent               } from "./robotlegs/bender/framework/api/PinEvent";
+
+/**
+ * Framework Implementation
+ */
+export { ConfigManager          } from "./robotlegs/bender/framework/impl/ConfigManager";
+export { Context                } from "./robotlegs/bender/framework/impl/Context";
+export { ExtensionInstaller     } from "./robotlegs/bender/framework/impl/ExtensionInstaller";
+export { Lifecycle              } from "./robotlegs/bender/framework/impl/Lifecycle";
+export { LifecycleTransition    } from "./robotlegs/bender/framework/impl/LifecycleTransition";
+export { Logger                 } from "./robotlegs/bender/framework/impl/Logger";
+export { LogManager             } from "./robotlegs/bender/framework/impl/LogManager";
+export { MessageDispatcher      } from "./robotlegs/bender/framework/impl/MessageDispatcher";
+export { ObjectProcessor        } from "./robotlegs/bender/framework/impl/ObjectProcessor";
+export { Pin                    } from "./robotlegs/bender/framework/impl/Pin";
+export { RobotlegsInjector      } from "./robotlegs/bender/framework/impl/RobotlegsInjector";
+export { UID                    } from "./robotlegs/bender/framework/impl/UID";
+
+/**
+ * Framework Functions
+ */
+export { applyHooks             } from "./robotlegs/bender/framework/impl/applyHooks";
+export { guardsApprove          } from "./robotlegs/bender/framework/impl/guardsApprove";
+export { safelyCallBack         } from "./robotlegs/bender/framework/impl/safelyCallBack";
 
 export { EventDispatcher        } from "./robotlegs/bender/events/EventDispatcher";
 export { IEventDispatcher       } from "./robotlegs/bender/events/IEventDispatcher";
-
-/**
- * Functions
- */
-export { guardsApprove          } from "./robotlegs/bender/framework/impl/guardsApprove";
-export { applyHooks             } from "./robotlegs/bender/framework/impl/applyHooks";
-
-
-/**
- * MVCS
- */
-export { Mediator               } from "./robotlegs/bender/bundles/mvcs/Mediator";
-export { IMediator              } from "./robotlegs/bender/extensions/mediatorMap/api/IMediator";
-export { Command                } from "./robotlegs/bender/bundles/mvcs/Command";
-export { ICommand               } from "./robotlegs/bender/extensions/commandCenter/api/ICommand";
 
 /*
  * Dependency injection
