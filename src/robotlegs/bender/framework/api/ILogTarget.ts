bc68a67aedfc32e5a9273941ae091d5a779bbd84
Tiago Schenkel <tschenkel@goodgamestudios.com>
2016-08-12 15:25:49 +0200

    Parameter message should be a primitive type.

--- src/robotlegs/bender/framework/api/ILogTarget.ts
+++ src/robotlegs/bender/framework/api/ILogTarget.ts
@@ -18,5 +18,5 @@ export interface ILogTarget {
      * @param message The log message
      * @param params Message parameters
      */
-    log(source: any, level: number, timestamp: number, message: String, params: any[]): void;
+    log(source: any, level: number, timestamp: number, message: string, params: any[]): void;
 }
