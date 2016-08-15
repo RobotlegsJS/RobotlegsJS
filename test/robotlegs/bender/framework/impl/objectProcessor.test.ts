0ef5e2c572e50652b967074bfa003ba3d4e83ca6
Tiago Schenkel <tschenkel@goodgamestudios.com>
2016-08-15 11:00:20 +0200

    Added Unit Tests for ObjectProcessor class.

---
+++ test/robotlegs/bender/framework/impl/objectProcessor.test.ts
@@ -0,0 +1,83 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2011 the original author or authors. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
+/// <reference path="../../../../../node_modules/reflect-metadata/reflect-metadata.d.ts" />
+/// <reference path="../../../../../typings/index.d.ts" />
+
+import "reflect-metadata";
+
+import { assert } from "chai";
+
+import { ObjectProcessor } from "../../../../../src/robotlegs/bender/framework/impl/ObjectProcessor";
+import { instanceOfType } from "../../../../../src/robotlegs/bender/extensions/matching/instanceOfType";
+
+import { TestObject } from "./objectSupport/TestObject";
+
+describe("ObjectProcessor", () => {
+
+    let objectProcessor: ObjectProcessor;
+
+    beforeEach(() => {
+        objectProcessor = new ObjectProcessor();
+    });
+
+    afterEach(() => {
+        objectProcessor = null;
+    });
+
+    it("handler handles object", () => {
+        let expected: Object = new TestObject("TestObject");
+        let actual: Object = null;
+        objectProcessor.addObjectHandler(instanceOfType(TestObject), function(object: Object): void {
+            actual = object;
+        });
+        objectProcessor.processObject(expected);
+        assert.equal(actual, expected);
+    });
+
+    it("handler does not handle wrong object", () => {
+        let actual: Object = null;
+        objectProcessor.addObjectHandler(instanceOfType(TestObject), function(object: Object): void {
+            actual = object;
+        });
+        objectProcessor.processObject({});
+        assert.isNull(actual);
+    });
+
+    it("handlers handle object", () => {
+        let expected: string[] = ["handler1", "handler2", "handler3"];
+        let actual: string[] = [];
+        objectProcessor.addObjectHandler(instanceOfType(TestObject), function(object: Object): void {
+            actual.push("handler1");
+        });
+        objectProcessor.addObjectHandler(instanceOfType(TestObject), function(object: Object): void {
+            actual.push("handler2");
+        });
+        objectProcessor.addObjectHandler(instanceOfType(TestObject), function(object: Object): void {
+            actual.push("handler3");
+        });
+        objectProcessor.processObject(new TestObject("TestObject"));
+        assert.deepEqual(actual, expected);
+    });
+
+    it("remove all handlers", () => {
+        let expected: string[] = [];
+        let actual: string[] = [];
+        objectProcessor.addObjectHandler(instanceOfType(TestObject), function(object: Object): void {
+            actual.push("Handler should not fire after call to removeAllHandlers.");
+        });
+        objectProcessor.addObjectHandler(instanceOfType(TestObject), function(object: Object): void {
+            actual.push("Handler should not fire after call to removeAllHandlers.");
+        });
+        objectProcessor.addObjectHandler(instanceOfType(TestObject), function(object: Object): void {
+            actual.push("Handler should not fire after call to removeAllHandlers.");
+        });
+        objectProcessor.removeAllHandlers();
+        objectProcessor.processObject(new TestObject("TestObject"));
+        assert.deepEqual(actual, expected);
+    });
+});
---
+++ test/robotlegs/bender/framework/impl/objectSupport/TestObject.ts
@@ -0,0 +1,14 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2012 the original author or authors. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
+export class TestObject {
+    constructor(
+        public name: string
+    ) {
+        this.name = name;
+    }
+}
