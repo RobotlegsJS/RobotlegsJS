cdbbf91e4fb2f070e0f0a0613bb77bb9d99806f4
Tiago Schenkel <tschenkel@goodgamestudios.com>
2016-08-17 11:50:41 +0200

    Added CallbackConfig support class.

---
+++ test/robotlegs/bender/framework/impl/contextSupport/CallbackConfig.ts
@@ -0,0 +1,33 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2012 the original author or authors. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file 
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
+import { IConfig } from "../../../../../../src/robotlegs/bender/framework/api/IConfig";
+
+export class CallbackConfig implements IConfig {
+
+    /*============================================================================*/
+    /* Private Properties                                                         */
+    /*============================================================================*/
+
+    private _callback: Function;
+
+    /*============================================================================*/
+    /* Constructor                                                                */
+    /*============================================================================*/
+
+    constructor(callback: Function) {
+        this._callback = callback;
+    }
+
+    /*============================================================================*/
+    /* Public Functions                                                           */
+    /*============================================================================*/
+
+    public configure(): void {
+        this._callback();
+    }
+}
