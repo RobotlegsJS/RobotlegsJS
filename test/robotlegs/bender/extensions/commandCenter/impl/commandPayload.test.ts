c601115df75c462b14c1a1e832df2c28e6bcfa60
Tiago Schenkel <tschenkel@goodgamestudios.com>
2016-08-31 13:45:00 +0200

    Added Unit Tests for CommandPayload class.

---
+++ test/robotlegs/bender/extensions/commandCenter/impl/commandPayload.test.ts
@@ -0,0 +1,90 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2009-2013 the original author or authors. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
+/// <reference path="../../../../../../node_modules/reflect-metadata/reflect-metadata.d.ts" />
+/// <reference path="../../../../../../typings/index.d.ts" />
+
+import "reflect-metadata";
+
+import { assert } from "chai";
+
+import { CommandPayload } from "../../../../../../src/robotlegs/bender/extensions/commandCenter/api/CommandPayload";
+
+describe("CommandPayload", () => {
+
+    let subject: CommandPayload;
+
+    function createConfig(values: any[] = null, classes: any[] = null): CommandPayload {
+        return subject = new CommandPayload(values, classes);
+    }
+
+    beforeEach(() => {
+        ;
+    });
+
+    afterEach(() => {
+        subject = null;
+    });
+
+    it("test values by default null", () => {
+        createConfig();
+        assert.isNull(subject.values);
+    });
+
+    it("test classes by default null", () => {
+        createConfig();
+        assert.isNull(subject.classes);
+    });
+
+    it("test values are stored", () => {
+        let expected: any[] = ["string", 0];
+        createConfig(expected);
+        assert.deepEqual(subject.values, expected);
+    });
+
+    it("test classes are stored", () => {
+        let expected: any[] = [String, Number];
+        createConfig(null, expected);
+        assert.deepEqual(subject.classes, expected);
+    });
+
+    it("test adding stores values", () => {
+        createConfig();
+
+        subject.addPayload("string", String);
+
+        let hasValue: boolean = subject.values.indexOf("string") > -1;
+        assert.isTrue(hasValue);
+    });
+
+    it("test adding stores classes", () => {
+        createConfig();
+
+        subject.addPayload("string", String);
+
+        let hasClass: boolean = subject.classes.indexOf(String) > -1;
+        assert.isTrue(hasClass);
+    });
+
+    it("test adding stores in lockstep", () => {
+        createConfig(["string", 0], [String, Number]);
+
+        let value: Object = {};
+
+        subject.addPayload(value, Object);
+
+        let valueIndex: Number = subject.values.indexOf(value);
+        let classIndex: Number = subject.classes.indexOf(Object);
+
+        assert.equal(valueIndex, classIndex);
+    });
+
+    it("can ask for length without classes", () => {
+        createConfig();
+        assert.equal(subject.length, 0);
+    });
+});
