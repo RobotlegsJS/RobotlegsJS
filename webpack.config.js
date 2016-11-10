ac5bebec57c3ce3a88ce9349b49c00b7f3cef768
Tiago Schenkel <tschenkel@goodgamestudios.com>
2016-11-10 17:29:38 +0100

    update ident size rules on .editorconfig

--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,43 @@ const path = require('path');
 
 module.exports = (function(options) {
 
-    return {
-        entry: {
-            main: path.join(__dirname, "src/index.ts")
-        },
+  return {
+    entry: {
+      main: path.join(__dirname, "src/index.ts")
+    },
 
-        output: {
-            path: path.join(__dirname, "dist"),
-            filename: "bundle.js"
-        },
+    output: {
+      path: path.join(__dirname, "dist"),
+      filename: "bundle.js"
+    },
 
-        devtool: 'inline-source-map',
+    devtool: 'inline-source-map',
 
-        module: {
-            rules: [
+    module: {
+      rules: [
 
-                { test: /\.ts$/, loader: "ts-loader" },
-                {
-                    test: /^(.(?!\.test))*\.ts$/,
-                    loader: "istanbul-instrumenter-loader",
-                    query: {
-                        embedSource: true
-                    },
-                    enforce: "post"
-                }
-            ]
-        },
-
-        plugins: [
-            // new webpack.optimize.UglifyJsPlugin()
-            new webpack.SourceMapDevToolPlugin({ test: /\.ts$/i })
-        ],
-
-        resolve: {
-            extensions: ['.ts', '.js', '.json'],
-            alias: {
-                // sinon: 'sinon/pkg/sinon'
-            }
+        { test: /\.ts$/, loader: "ts-loader" },
+        {
+          test: /^(.(?!\.test))*\.ts$/,
+          loader: "istanbul-instrumenter-loader",
+          query: {
+            embedSource: true
+          },
+          enforce: "post"
         }
+      ]
+    },
+
+    plugins: [
+      // new webpack.optimize.UglifyJsPlugin()
+      new webpack.SourceMapDevToolPlugin({ test: /\.ts$/i })
+    ],
+
+    resolve: {
+      extensions: ['.ts', '.js', '.json'],
+      alias: {
+        // sinon: 'sinon/pkg/sinon'
+      }
     }
+  }
 })();
