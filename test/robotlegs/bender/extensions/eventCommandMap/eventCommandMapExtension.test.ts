60d4cc18aac66a8ec10f60c1fe8c48cc992a79c4
Tiago Schenkel <tiago.schenkel@gmail.com>
2017-11-07 10:57:31 +0100

    add unit tests for EventCommandMapExtension class

---
+++ test/robotlegs/bender/extensions/eventCommandMap/eventCommandMapExtension.test.ts
@@ -0,0 +1,49 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2017 RobotlegsJS. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
+import "../../../../entry";
+
+import { assert } from "chai";
+
+import { IContext } from "../../../../../src/robotlegs/bender/framework/api/IContext";
+import { Context } from "../../../../../src/robotlegs/bender/framework/impl/Context";
+
+import { IEventDispatcher } from "../../../../../src/robotlegs/bender/events/api/IEventDispatcher";
+import { EventDispatcher } from "../../../../../src/robotlegs/bender/events/impl/EventDispatcher";
+
+import { IEventCommandMap } from "../../../../../src/robotlegs/bender/extensions/eventCommandMap/api/IEventCommandMap";
+import { EventCommandMap } from "../../../../../src/robotlegs/bender/extensions/eventCommandMap/impl/EventCommandMap";
+import { EventCommandMapExtension } from "../../../../../src/robotlegs/bender/extensions/eventCommandMap/EventCommandMapExtension";
+
+describe("EventCommandMapExtension", () => {
+    let eventDispatcher: IEventDispatcher;
+    let context: IContext;
+
+    beforeEach(() => {
+        eventDispatcher = new EventDispatcher();
+        context = new Context();
+        context.injector
+            .bind(IEventDispatcher)
+            .toConstantValue(new EventDispatcher());
+    });
+
+    afterEach(() => {
+        context.destroy();
+        context = null;
+        eventDispatcher = null;
+    });
+
+    it("eventCommandMap_is_mapped_into_injector", () => {
+        let actual: IEventCommandMap = null;
+        context.install(EventCommandMapExtension);
+        context.whenInitializing(function(): void {
+            actual = context.injector.get<IEventCommandMap>(IEventCommandMap);
+        });
+        context.initialize();
+        assert.instanceOf(actual, EventCommandMap);
+    });
+});
