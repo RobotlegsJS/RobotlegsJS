7d60647400ee977fd86dc133af14c12e54886407
Tiago Schenkel <tiago.schenkel@gmail.com>
2018-04-02 21:08:00 +0200

    add copyright header

--- src/robotlegs/bender/framework/impl/ObjectMatcher.ts
+++ src/robotlegs/bender/framework/impl/ObjectMatcher.ts
@@ -1,3 +1,10 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2017 RobotlegsJS. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
 import { IMatcher } from "../api/IMatcher";
 
 /**
