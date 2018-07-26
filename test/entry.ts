ac4536ae0427e3a893c75d177f247f62ac2815b7
Tiago Schenkel <tiago.schenkel@gmail.com>
2018-07-26 23:37:55 +0200

    adopts year-agnostic copyright message

--- test/entry.ts
+++ test/entry.ts
@@ -1,3 +1,10 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2017-present, RobotlegsJS. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
 /// <reference types="mocha" />
 
 import "jsdom-global";
